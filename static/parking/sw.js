ad25bc3db24baafe2366629132a0744da18a0524
Yusuf Elsharawy <yusufse-2004@hotmail.com>
2024-08-22 16:37:20 -0400

    Fixed shadowing of `error`, trying service notifications from button

--- static/parking/sw.js
+++ static/parking/sw.js
@@ -1,1 +1,18 @@
-console.log("Hello World!");
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open('your - cache - name').then(cache => {
+            return cache.addAll([
+                '/style.css',
+                'index.html'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
